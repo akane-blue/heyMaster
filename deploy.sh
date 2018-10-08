72b8d79d904e09577a7afe3fc2edf7e4c810fd75
lindwurm <lindwurm.q@gmail.com>
2018-10-08 23:34:50 +0900

    Separate update and deploy scripts
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+COMMITHASH=$(git ls-remote https://github.com/lindwurm/mastodon.git HEAD | head -c 7)
+cd mastodon
+echo "[${COMMITHASH}] でっぷろーい！" | toot
+docker-compose up -d
+
+while true; do
+        DonAlive=$(curl -s -o /dev/null -I -w "%{http_code}\n" https://mstdn.maud.io/)
+        if [ $DonAlive -eq 302 ]; then
+                break
+        fi
+                echo "しっぱい… 5s後に再試行するよ"
+                sleep 5s
+done
+
+echo "[${COMMITHASH}] だん!" | toot
+
--- update.sh
+++ update.sh
@@ -9,17 +9,4 @@ docker-compose build
 docker-compose run --rm web rails db:migrate
 echo "[${COMMITHASH}] ぷりこんぱいる？" | toot
 docker-compose run --rm web rails assets:precompile
-echo "[${COMMITHASH}] でっぷろーい！" | toot
-docker-compose up -d
-
-while true; do
-        DonAlive=$(curl -s -o /dev/null -I -w "%{http_code}\n" https://mstdn.maud.io/)
-        if [ $DonAlive -eq 302 ]; then
-                break
-        fi
-                echo "しっぱい… 5s後に再試行するよ"
-                sleep 5s
-done
-
-echo "[${COMMITHASH}] だん!" | toot
-
+echo "[${COMMITHASH}] 準備できたよ！" | toot
