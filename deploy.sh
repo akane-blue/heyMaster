2cb5c5d6c1bb0c3e89017a845dce7f7739a4d3a2
lindwurm <lindwurm.q@gmail.com>
2019-02-07 11:57:31 +0900

    deploy: refactor

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 COMMITHASH=$(git ls-remote https://github.com/lindwurm/mastodon.git HEAD | head -c 7)
 cd mastodon
@@ -7,13 +7,12 @@ sleep 5s
 docker-compose up -d --remove-orphans
 
 while true; do
-        DonAlive=$(curl -s -o /dev/null -I -w "%{http_code}\n" https://mstdn.maud.io/)
-        if [ $DonAlive -eq 302 ]; then
-                break
-        fi
-                echo "しっぱい… 5s後に再試行するよ"
-                sleep 5s
+    DonAlive=$(curl -s -o /dev/null -I -w "%{http_code}\n" https://mstdn.maud.io/)
+    if [ $DonAlive -eq 302 ]; then
+        break
+    fi
+        echo "しっぱい… 5s後に再試行するよ"
+        sleep 5s
 done
 
 echo "[${COMMITHASH}] だん!" | toot
-
